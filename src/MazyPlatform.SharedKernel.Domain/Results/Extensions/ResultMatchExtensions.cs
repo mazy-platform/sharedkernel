739bd6efc86bad0cd561909d5281c386f055d503
dystar <dystarik@gmail.com>
2025-11-27 23:10:50 +0700

    feat(result): Добавить расширения для обработки Result
    Добавить новый файл ResultMatchExtensions.cs с методами расширения
    для работы с объектами Result и Result<TValue>. Реализовать методы
    Match и MatchAsync для синхронной и асинхронной обработки успешных
    результатов и ошибок. Методы позволяют использовать паттерн Match
    для упрощения обработки результатов.

---
+++ src/MazyPlatform.SharedKernel.Domain/Results/Extensions/ResultMatchExtensions.cs
@@ -0,0 +1,37 @@
+﻿using MazyPlatform.SharedKernel.Domain.Results.Errors;
+
+namespace MazyPlatform.SharedKernel.Domain.Results.Extensions;
+
+
+public static class ResultMatchExtensions
+{
+    public static T Match<T>(this Result result, Func<T> onSuccess, Func<ErrorCollection, T> onFailure)
+    {
+        return result.IsSuccess
+                ? onSuccess()
+                : onFailure(result.Errors);
+    }
+
+    public static T Match<T, TValue>(this Result<TValue> result, Func<TValue, T> onSuccess, Func<ErrorCollection, T> onFailure)
+    {
+        return result.IsSuccess
+                ? onSuccess(result.Value)
+                : onFailure(result.Errors);
+    }
+
+    public static async Task<T> MatchAsync<T>(this Task<Result> resultTask, Func<T> onSuccess, Func<ErrorCollection, T> onFailure)
+    {
+        var result = await resultTask.ConfigureAwait(false);
+        return result.IsSuccess
+            ? onSuccess()
+            : onFailure(result.Errors);
+    }
+
+    public static async Task<T> MatchAsync<T, TValue>(this Task<Result<TValue>> resultTask, Func<TValue, T> onSuccess, Func<ErrorCollection, T> onFailure)
+    {
+        var result = await resultTask.ConfigureAwait(false);
+        return result.IsSuccess
+            ? onSuccess(result.Value)
+            : onFailure(result.Errors);
+    }
+}
